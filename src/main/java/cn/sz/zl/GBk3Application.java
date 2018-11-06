01777248053b62741ebbb8498365fd716899e61b
Wbj19941020@163.com <Wbj19941020@163.com>
2018-11-06 15:42:44 +0800

    create new project

---
+++ src/main/java/cn/sz/zl/GBk3Application.java
@@ -0,0 +1,12 @@
+package cn.sz.zl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GBk3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GBk3Application.class, args);
+	}
+}
